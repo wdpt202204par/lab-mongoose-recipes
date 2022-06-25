76714a9056214df6f8bc5000551ca45de61c32ac
Benjamin Amzzough <amzzough.benjamin@yahoo.fr>
2022-06-25 17:09:11 +0200

    done

--- index.js
+++ index.js
@@ -16,8 +16,57 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
+    Recipe.create({
+      title: "Pizza Regina",
+      level:
+        "Amateur Chef",
+      ingredients: ["pizza dough", "tomato", "mozzarella", "ham", "olive", "mushrooms"],
+      cuisine: "Italian",
+      dishType: "main_course",
+      image: "https://img.cuisine-etudiant.fr/image/recette/800500/30a08382dd90bcc6e242e84055b2578cf12df04b-veritable-pizza-regina-buonissimo.jpg",
+      duration: 15,
+      creator: "Peppe",
+      created: 1780
+    })
+      .then(function () {
+        console.log("pizza created")
+      })
+      .catch(function () {
+        console.log("oops pizza error")
+      })
+    Recipe.create(data)
+      .then(function () {
+        console.log("data created")
+        Recipe.findOneAndUpdate({
+          title: "Rigatoni alla Genovese"
+        }, { duration: 100 })
+          .then(function () {
+            console.log("data updated")
+            Recipe.deleteOne({
+              title: "Carrot Cake"
+            })
+              .then(function () {
+                console.log("Carrot deleted")
+                mongoose.connection.close()
+                console.log("connection close")
+              })
+              .catch(function () {
+                console.log("Carrot not deleted")
+              })
+          })
+          .catch(function () {
+            console.log("oops data not updated")
+          })
+      })
+      .catch(function () {
+        console.log("oops data error")
+      })
+  }
+  )
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })
+
+
+
+

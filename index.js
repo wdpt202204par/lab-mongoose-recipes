5e4339213f2fb5123221fc483f066380dcc82934
Cécile Deshayes-Guinard <cecile.deshayes.guinard@gmail.com>
2022-06-27 21:26:57 +0200

    in progress

--- index.js
+++ index.js
@@ -1,44 +1,64 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
     Recipe.create({
-      title: String,
-      level: String,
-      ingredient: Array,
-      cuisine: String,
-      dishType: String,
-      image: String,
-      duration: Number,
-      creator: String,
-      created: String
+      title: 'Tiramisu',
+      level: 'Easy Peasy',
+      ingredient: ['cheese',
+                   'eggs',
+                   'sugar'],
+      cuisine: 'international',
+      dishType: 'dessert',
+      image: 'none',
+      duration: 15,
+      creator: 'Cécile',
+      created: 2005,
     })
       .then(function (newRecipe) {
-        console.log('1 recipe created:', newRecipe.title)
+        console.log("1 recipe created:", newRecipe.title);
       })
-      .catch((err) => {console.log(err)})
-    
-    // Recipe.insertMany(data) // j'insère tout les recipes de data.json en prenant comme modèle newRecipe
-    //   .then(function (recipes) {
-    //     console.log(`${recipes.length} recipes ont ete crees`)
-    //   })
-    //   .catch(err => console.log('oops', err))
+      .catch((err) => {
+        console.log(err);
+      });
+
+    Recipe.insertMany(data) // j'insère tout les recipes de data.json en prenant comme modèle newRecipe
+      .then(function (recipes) {
+        console.log(`${recipes.length} recipes ont ete crees`);
+
+        // itérartion 3 :
+
+        // recipes: [ {_id: , title: , ...}, {}, {}]
+        recipes.forEach(function (el) {
+          console.log(el.title)
+        })
+
+        // itération 4 :
+
+        Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration:100})
+        .then(function(oneUpdate) {
+          console.log('data changed with succes')})
+        .catch(function(oneUpdateMiss) {
+          console.log('not changed')});
+      })
+
+      .catch((err) => console.log("oops", err));
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
